ec908abc9964266e34a8d3b34075ce146b840f9c
hopesoh <sonia.ribeiro@usp.br>
2021-02-04 14:26:42 -0300

    [Fourth Section] Use Java operators - comparing

---
+++ certification/course3/section4/TestaComparacao.java
@@ -0,0 +1,21 @@
+class TestaComparacao {
+    public static void main(String[] args) {
+
+        int idade = 25;
+        System.out.println(idade = 24);
+
+        System.out.println(1.0f == 1.0d);
+        System.out.println(1 == 1.0);
+
+        System.out.println(true == true);
+        System.out.println(true != false);
+
+        System.out.println(1 == 1);
+        System.out.println(1 != 1);
+
+        System.out.println(2 >= 1);
+        System.out.println(2 <= 1);
+        System.out.println(2 > 1);
+        System.out.println(2 < 1);
+    }
+}
---
+++ certification/course3/section4/section-4.md
@@ -0,0 +1,17 @@
+# Operadores de comparação
+
+Dois principais operadores de comparação:
+- **==:** Um valor é igual ao outro
+- **!=:** Um valor é diferente do outro
+
+Além disso, existem os:
+- **>=:** Um número é maior ou igual ao outro
+- **>:** Um número é maior que o outro
+- **<=:** Um número é menor ou igual ao outro
+- **<:** Um número é menor que o outro
+
+Uma comparação não leva em consideração, no caso dos tipos primitivos numéricos, o tipo do valor.
+
+**Tomar cuidado com o igual único.**
+
+
