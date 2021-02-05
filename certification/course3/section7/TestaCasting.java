ad54f8241ebd4d6c936142c972f79082a361c7e5
hopesoh <sonia.ribeiro@usp.br>
2021-02-05 15:21:21 -0300

    [Seventh Section] Casting

---
+++ certification/course3/section7/TestaCasting.java
@@ -0,0 +1,13 @@
+class TestaCasting {
+    public static void main(String[] args) {
+
+        double d = - 15.3;
+        int i = (int) d;
+        System.out.println(i);
+
+        long l = 151515151515151L;
+        int i2 = (int) l;
+        System.out.println(i2);
+
+    }
+}
---
+++ certification/course3/section7/section-7.md
@@ -0,0 +1,3 @@
+# Casting
+
+- O casting é a maneira que usamos para moldar uma variável de um tipo em outro. Nós estamos avisando o compilador que sabemos da possibilidade de perda de precisão ou truncamento, mas nós realmente queremos fazer isso.
