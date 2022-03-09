aa0d8004856c53e66c6df9935ccf5c026c7ccecb
hopesoh <sonia.ribeiro@alumni.usp.br>
2022-03-09 16:56:35 -0300

    Add more module exercises

---
+++ certification/module/multiplePurposeModule/module-info.java
@@ -0,0 +1,5 @@
+module zoo.tours.hybrid {
+	requires zoo.tours.api;
+	provides zoo.tours.api.Tour with zoo.tours.hybrid.QuickTourImpl;
+	uses zoo.tours.api.Tour;
+}
---
+++ certification/module/multiplePurposeModule/zoo/tours/hybrid/QuickTourImpl.java
@@ -0,0 +1,19 @@
+package zoo.tours.hybrid;
+
+import zoo.tours.api.*;
+
+public class QuickTourImpl implements Tour {
+	public String name() {
+		return "Short tour";
+	}
+
+	public int length() {
+		return 30;
+	}
+
+	public Souvenir getSouvenir() {
+		Souvenir gift = new Souvenir();
+		gift.setDescription("keychain");
+		return gift;
+	}
+}
